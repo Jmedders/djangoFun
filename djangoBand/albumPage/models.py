100e6ec5e26be8ac3db5441df1d7139f2bd92ca4
Jeff <medders.jeffrey@gmail.com>
2017-02-22 14:16:16 -0700

    displaying views of database info

--- djangoBand/albumPage/models.py
+++ djangoBand/albumPage/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Album(models.Model):
+    title = models.CharField(max_length = 200)
+    band = models.CharField(max_length = 200)
+    year = models.SmallIntegerField()
+
+    def __str__(self):
+        return self.title
--- djangoBand/albumPage/urls.py
+++ djangoBand/albumPage/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url, include
-from . import views
+from django.views.generic import ListView, DetailView
+from albumPage.models import Album
 
 urlpatterns = [
-    url(r'^$', views.index, name="index")
+    url(r'^$', ListView.as_view(queryset = Album.objects.all().order_by("band"),
+    template_name = "album/album.html"))
 ]
